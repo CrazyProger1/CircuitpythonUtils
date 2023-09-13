7cbce420eac40cdf74cfa94f1cdb059b549bdbb7
CrazyProger1 <crazyproger1@gmail.com>
2023-09-13 11:05:47 +0300

    Add: clear cache & cache tests

---
+++ tests/cache.py
@@ -0,0 +1,29 @@
+import unittest
+from utils import cache
+
+
+@cache.cache
+def test(*args, **kwargs):
+    return dict({1: 1})
+
+
+@cache.cache
+def test2():
+    pass
+
+
+class TestCache(unittest.TestCase):
+    def test_cache_decorator(self):
+        self.assertIs(test(), test())
+        self.assertIs(test('abc'), test('abc'))
+        self.assertIs(test('abc', test=123), test('abc', test=123))
+
+    def test_clear_cache(self):
+        result = test('abc')
+        cache.clear_cache(test)
+        result2 = test('abc')
+        self.assertIsNot(result, result2)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utils/cache.py
+++ utils/cache.py
@@ -1,6 +1,10 @@
 """Circuitpython cache implementations."""
 
 
+def clear_cache(func):
+    func(reset_cache=True)
+
+
 def cache(func):
     """Cache decorator implementation."""
 
@@ -8,6 +12,11 @@ def cache(func):
 
     def wrapper(*args, **kwargs):
         nonlocal result_cache
+
+        if kwargs.get('reset_cache'):
+            result_cache.clear()
+            return
+
         key = (func, args, frozenset(kwargs.items()))
 
         if key in result_cache:
